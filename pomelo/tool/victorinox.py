1fc4db69c453e35367d1b4b0ce20f074d55e6714
Zhiyi Luo <jessieluo1991@gmail.com>
2016-08-10 19:19:08 +0800

    add code2pdf

---
+++ pomelo/tool/victorinox.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import re
+
+def txt2pdf(textfn, ps_prefix=None, pdf_prefix=None):
+    """
+    Convert a .txt file to .pdf file
+    >>> txt2pdf('/home/luozhiyi/code/pyqt-ch04.txt')
+    """
+    text_prefix = os.path.splitext(textfn)[0]
+    psfn = text_prefix+'.ps' if not ps_prefix else ps_prefix+'.ps'
+    pdffn = text_prefix+'.pdf' if not pdf_prefix else pdf_prefix+'.pdf'
+    try:
+        os.system("enscript -p {0} {1}".format(psfn, textfn))
+        os.system("ps2pdf {0} {1}".format(psfn, pdffn))
+    except Exception as e:
+        return "Failed, please check if you've installed `enscript` and `ps2pdf` command:\n{}".format(e)
+
+def copyTextFileTo(fn, outf, logfn=False):
+    if logfn: outf.write('\n\n\n####################################\n'+fn+'\n####################################\n')
+    with open(fn) as f:
+        for line in f: outf.write(line)
+
+def collectCodes(source_dir, outputfn,
+                 filepats=[re.compile('\w+\.pyw?$'),re.compile('\w+\.(cpp|h|cc?)$'),
+                           re.compile('\w+\.java$'),re.compile('\w+\.scala$')],
+                 filterpats = [re.compile('.+\/build\/.+'), re.compile('.+\/UI[\/]?$'), re.compile('.+\/youtub-dl[\/]?$')]):
+    """
+    >>> collect(source_dir, outputfn)
+    """
+    if not os.path.exists(source_dir):
+        raise ValueError("Not found {}".format(source_dir))
+    with open(outputfn, 'w') as outf:
+        for path, dirlist, filelist in os.walk(source_dir):
+            for filename in filelist:
+                if any([pat.match(path) for pat in filterpats]): continue
+                if any([pat.match(filename) for pat in filepats]):
+                    fn = os.path.join(path, filename)
+                    copyTextFileTo(fn, outf, True)
+
+def codes2pdf(source_dir, pdf_prefix):
+    """
+    collect a project source codes into a pdf file
+    """
+    textfn = pdf_prefix+'.txt'
+    collectCodes(source_dir, textfn)
+    txt2pdf(textfn)
