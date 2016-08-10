b245b6632043295c44c2090ff3bcdf73f3e027c4
Zhiyi Luo <jessieluo1991@gmail.com>
2016-08-10 19:17:47 +0800

    code2pdf

--- pomelo/corpora/PlainCorpus.py
+++ pomelo/corpora/PlainCorpus.py
@@ -1,10 +1,15 @@
 import sys,os
+import re
+import random
 import numpy as np
 from six.moves import xrange
-
+from gensim import utils
+from collections import defaultdict
 
 class PlainCorpus(object):
     def __init__(self, corpus_fn):
+        if not os.path.exists(corpus_fn):
+            raise FileNotFoundError("Not Found this '{}' corpus! Please check!".format(corpus_fn))
         self.corpus_fn = corpus_fn
         self.constructed = False
 
@@ -17,6 +22,29 @@ class PlainCorpus(object):
         with open(self.corpus_fn) as f:
             for line in f: length += 1
         return length
+
+    def corpus_split(self, fnames, props):
+        size = self.corpus_size if self.constructed else self.get_corpus_length()
+        sids = list(range(size))
+        random.shuffle(sids)
+        start = 0
+        d = defaultdict(int)
+        for i,num in enumerate([int(p*size) for p in props]):
+            for k in sids[start:start+num]: d[k] = i
+            start += num
+        with open(self.corpus_fn) as f:
+            ff = [open(fn,'w') for fn in fnames]
+            for i,line in enumerate(f):
+                ff[d[i]].write(line)
+            for fl in ff: fl.close()
+
+    def create_vocab(self, fname):
+        wordset = set()
+        with open(self.corpus_fn) as f, open(fname,'w') as outf:
+            for line in f:
+                for token in line.strip().split():
+                    wordset.add(token)
+            for word in wordset: outf.write(word+"\n")
 
     def sample_corpus(self, sample_size, sample_fn):
         """
@@ -37,3 +65,30 @@ class PlainCorpus(object):
                     line = f.readline()
                     cnt += 1
                 outf.write(line)
+
+    def tokenize(self,content, BytesOrNot=False):
+        """
+        Tokenize a piece of text.
+        Return list of tokens as utf8 bytestrings. Ignore words shorted than 2 or longer
+        that 15 characters (not bytes!).
+        """
+    # https://github.com/RaRe-Technologies/gensim/blob/develop/gensim/corpora/wikicorpus.py#L166
+    # TODO maybe ignore tokens with non-latin characters? (no chinese, arabic, russian etc.)
+        if BytesOrNot:
+            return [token.encode('utf8') for token in utils.tokenize(content, lower=True, errors='ignore')
+                if 2 <= len(token) <= 15 and not token.startswith('_')] # return a list of bytes of characters
+        else: return list(utils.tokenize(content, lower=True, errors='ignore')) # return a list of strings
+
+    def get_texts(self):
+        PAT_DG = re.compile('(\d+,\d+)|(\d+\.\d+)|(\d+)', re.UNICODE)
+        DIGIT = "dddggg"
+        with open(self.corpus_fn) as f:
+            for line in f: # for each sentence
+                line = self.replace_tag(line, PAT_DG, DIGIT)
+                yield self.tokenize(line)
+
+    def replace_tag(self, line, pat, tag):
+        for match in pat.finditer(line.strip()):
+            line = line.replace(match.group(), tag)
+        return line
+    # (\d+,\d+)|(\d+\.\d+)|(\d+)
--- pomelo/test/test_PlainCorpus.py
+++ pomelo/test/test_PlainCorpus.py
@@ -7,9 +7,11 @@ sys.path.insert(0, os.path.abspath('../..'))
 from pomelo.corpora import PlainCorpus
 
 output_dir = '/home/luozhiyi/projects/WhatHappen/data/'
+
 class PlainCorpusTestCase(unittest.TestCase):
     def setUp(self):
-        self.corpus = PlainCorpus.PlainCorpus('/home/luozhiyi/data/causal/Causal_Sentences.txt')
+        self.big_corpus = PlainCorpus.PlainCorpus('/home/luozhiyi/data/causal/Causal_Sentences.txt')
+        self.small_corpus = PlainCorpus.PlainCorpus('/home/luozhiyi/projects/WhatHappen/data/sampled_causal_sentences.txt')
 
     def test_sample_plain_corpus(self):
         corpus = self.corpus
@@ -18,6 +20,31 @@ class PlainCorpusTestCase(unittest.TestCase):
         corpus.sample_corpus(sample_size, sample_fn)
         assert os.path.exists(sample_fn) == True
         assert PlainCorpus.PlainCorpus(sample_fn).get_corpus_length() == sample_size
+
+    def test_tokenize(self):
+        with open('pp.txt') as f:
+            for line in f:
+                print(self.small_corpus.tokenize(line))
+
+    def test_get_texts(self):
+        DIGIT = "dddggg"
+        outfn = 'processed1.txt'
+        with open(outfn,'w') as outf:
+            for text in self.small_corpus.get_texts():
+                outf.write(' '.join(text).replace(DIGIT, "DG"))
+                outf.write('\n')
+
+    def test_corpus_split(self):
+        processed_corpus = PlainCorpus.PlainCorpus('/home/luozhiyi/projects/WhatHappen/data/processed.txt')
+        props = [0.05, 0.05, 0.9]
+        output_dir = "/home/luozhiyi/projects/WhatHappen/data/"
+        fnames = [output_dir+"ptb.test.txt", output_dir+"ptb.valid.txt", output_dir+"ptb.train.txt"]
+        processed_corpus.corpus_split(fnames, props)
+
+    def test_create_vocab(self):
+        processed_corpus = PlainCorpus.PlainCorpus('/home/luozhiyi/projects/WhatHappen/data/processed.txt')
+        output_dir = "/home/luozhiyi/projects/WhatHappen/data/"
+        processed_corpus.create_vocab(output_dir+"vocab.ptb.txt")
 
 def get_suite(cls, test_names):
     suite = unittest.TestSuite()
@@ -29,6 +56,9 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     #unittest.main()
     runner = unittest.TextTestRunner()
-    print(runner.run(
-            get_suite(PlainCorpusTestCase, ['test_sample_plain_corpus'])
-            ))
+    suite1 = get_suite(PlainCorpusTestCase, ['test_sample_plain_corpus'])
+    suite2 = get_suite(PlainCorpusTestCase, ['test_tokenize'])
+    suite3 = get_suite(PlainCorpusTestCase, ['test_get_texts'])
+    suite4 = get_suite(PlainCorpusTestCase, ['test_corpus_split'])
+    suite5 = get_suite(PlainCorpusTestCase, ['test_create_vocab'])
+    print(runner.run( suite5 ))
